6971d85c1bb985348bb99139a4114d75307f937a
glab <glabexp@gmail.com>
2022-03-01 22:13:45 +0100

    mouse pos to world coord

--- src/game.h
+++ src/game.h
@@ -81,6 +81,26 @@ glm::vec2 worldToScreen(uint32 x, uint32 y, uint32 tileSize) {
     return screenPos;
 }
 
+glm::vec2 worldToScreen2(uint32 x, uint32 y, uint32 tileSize) {
+    glm::vec2 screenPos(0.0f);
+
+    screenPos.x = ((float32)(x * 0.5 * tileSize) + (float32)(y * (-0.5) * tileSize)) - (float32)(tileSize / 2);
+    screenPos.y = (float32)(x * 0.25 * tileSize) + (float32)(y * 0.25 * tileSize);
+
+    return screenPos;
+}
+
+glm::vec2 screenToWorld(float32 x, float32 y, uint32 tileSize) {
+    /* x = 0.5 * ( screenX / tileWidthHalf + screenY / tileHeightHalf) */
+    /* y = 0.5 * (-screenX / tileWidthHalf + screenY / tileHeightHalf) */
+    glm::vec2 worldPos(0.0f);
+
+    worldPos.x = (float32)(0.5 * (x / (tileSize/2) + y / (tileSize/4)));
+    worldPos.y = (float32)(0.5 * (-x / (tileSize/2) + y / (tileSize/4)));
+
+    return worldPos;
+}
+
 void UpdateAndRender(Game* game, SpriteRenderer* renderer, Plateform_MouseEvent* mouse) {
     std::vector<Sprite> testList;
     testList.clear();
@@ -89,24 +109,24 @@ void UpdateAndRender(Game* game, SpriteRenderer* renderer, Plateform_MouseEvent*
     uint32 sizeY = 14;
     uint32 spriteSize = 100;
     //glm::vec2 cellCoordIso = glm::vec2(game->width / spriteSize, game->height / spriteSize); // 19,14
-    glm::vec2 origin = glm::vec2((float32)((game->width/2) - (spriteSize/2)), (float32)((game->height/3) - (spriteSize/2)));
+    glm::vec2 origin = glm::vec2((float32)(game->width/2), (float32)(game->height/3));
+    //glm::vec2 origin = glm::vec2(0.0f);
 
 
     //printf("mouse %.1f/%.1f\n", mouse->posX, mouse->posY);
     // 2. get cell from world space the mouse pos is in: (mouse.x / spriteSize) (mouse.y / spriteSze)
     glm::vec2 cell = glm::vec2((float32)(mouse->posX / spriteSize), (float32)(mouse->posY / spriteSize));
-    printf("cell %d/%d\n", (uint32)(cell.x), (uint32)(cell.y));
 
     // 3. get offset into the cell: (mouse.x % spriteSize) (mouse.y % spriteSize)
     //glm::vec2 cellOffset = glm::vec2(mouse->posX % spriteSize, mouse->posY % spriteSize);
     // .. Current cell hovered: (cell.x * spriteSize) (cell.y * spriteSize)
     // 4. Selected cell: (cell.y - origin.y) + (cell.x - origin.x)
     //                   (cell.y - origin.y) - (cell.x - origin.x)
-    glm::vec2 selectCell = glm::vec2((cell.y - origin.y) + (cell.x - origin.x), (cell.y - origin.y) - (cell.x - origin.x));
+    //glm::vec2 selectCell = glm::vec2((cell.y - origin.y) + (cell.x - origin.x), (cell.y - origin.y) - (cell.x - origin.x));
 
     for (uint32 y=0; y<sizeY; y++) {
         for (uint32 x=0; x<sizeX; x++) {
-            glm::vec2 screenPos = worldToScreen(x, y, spriteSize);
+            glm::vec2 screenPos = worldToScreen2(x, y, spriteSize);
             testList.push_back(InitSprite(GetTextureFromCache(TEXTURE_CACHE, "block"),
                                           glm::vec2(origin.x + screenPos.x, origin.y + screenPos.y),
                                           glm::vec2((float32)spriteSize),
@@ -121,11 +141,16 @@ void UpdateAndRender(Game* game, SpriteRenderer* renderer, Plateform_MouseEvent*
         DrawSprite(renderer, &testList[i], GetShaderFromCache(SHADER_CACHE, "sprite"));
 
     // 5. selected World = selectedCell worldToScreen convert
-    glm::vec2 selectedCellPos = worldToScreen((uint32)(mouse->posX / spriteSize), (uint32)(mouse->posY / spriteSize), spriteSize);
+    //printf("mouse %.1f/%.1f\n", mouse->posX, mouse->posY);
+    //printf("cell %d/%d\n", (uint32)(cell.x), (uint32)(cell.y));
+    glm::vec2 test = screenToWorld((float32)(mouse->posX - origin.x), (float32)(mouse->posY - origin.y), spriteSize);
+    printf("world %.1f/%.1f\n", test.x, test.y);
+    glm::vec2 selectedCellPos = worldToScreen2((uint32)(test.x), (uint32)(test.y), spriteSize);
+    printf("screen %.1f/%.1f\n", selectedCellPos.x, selectedCellPos.y);
     // 6. Draw block 2 at this pos.
     Sprite selected = InitSprite(GetTextureFromCache(TEXTURE_CACHE, "block2"),
-               glm::vec2(origin.x + selectedCellPos.x, origin.y + selectedCellPos.y),
-               glm::vec2((float32)spriteSize),
-               glm::vec3(1.0f));
+                                 glm::vec2(origin.x + selectedCellPos.x, origin.y + selectedCellPos.y),
+                                 glm::vec2((float32)spriteSize),
+                                 glm::vec3(1.0f));
     DrawSprite(renderer, &selected, GetShaderFromCache(SHADER_CACHE, "sprite"));
 }
