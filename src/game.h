0b925ee5d4fa54a00881ec18f0dace78e990094f
glab <glabexp@gmail.com>
2022-03-02 18:56:07 +0100

    cleanup test isometric grid

--- src/game.h
+++ src/game.h
@@ -6,6 +6,9 @@
 #include "renderer.h"
 #include "sprite.h"
 
+glm::vec2 worldToScreen(uint32 x, uint32 y, uint32 tileSize);
+glm::vec2 screenToWorld(float32 x, float32 y, uint32 tileSize);
+
 std::map<std::string, Shader> SHADER_CACHE;
 std::map<std::string, Texture2D> TEXTURE_CACHE;
 
@@ -19,6 +22,11 @@ enum GameState {
 struct Game {
     GameState state;
     uint32 width, height;
+
+    glm::vec2 origin;
+    glm::uvec2 world;
+    std::vector<Sprite> testList;
+    Sprite selectedTest;
 };
 
 Game InitGameAndLoadAssets(uint32 width, uint32 height) {
@@ -56,10 +64,36 @@ Game InitGameAndLoadAssets(uint32 width, uint32 height) {
     game.width = width;
     game.height = height;
 
+
+    game.world.x = 10;
+    game.world.y = 14;
+    uint32 spriteSize = 100;
+    game.origin = glm::vec2((float32)(width/2), (float32)(height/3));
+    game.testList.clear();
+
+    for (uint32 y=0; y<game.world.y; y++) {
+        for (uint32 x=0; x<game.world.x; x++) {
+            glm::vec2 screenPos = worldToScreen(x, y, spriteSize);
+            game.testList.push_back(InitSprite(GetTextureFromCache(TEXTURE_CACHE, "block"),
+                                               glm::vec2(game.origin.x + screenPos.x, game.origin.y + screenPos.y),
+                                               glm::vec2((float32)spriteSize),
+                                               glm::vec3(1.0f)));
+        }
+    }
+
+    game.selectedTest = InitSprite(GetTextureFromCache(TEXTURE_CACHE, "block5"),
+                                   glm::vec2(0.0f),
+                                   glm::vec2((float32)spriteSize),
+                                   glm::vec3(1.0f));
+    
     return game;
 }
 
 void ProcessInput(Game* game, Plateform_KeyboardEvent* keyboard, Plateform_MouseEvent* mouse, float32 dt) {
+    // test scroll
+    float32 scrollOffset = GetMouseScrollOffsetY(mouse);
+    printf("mouse scroll offset %.1f\n", scrollOffset);
+
     if (keyboard->isPressed[KEYBOARD::CRAP_KEY_ESCAPE])
         game->state = GAME_EXIT;
 }
@@ -68,26 +102,28 @@ void SoundSample(Game* game) {
     // ...
 }
 
-/* glm::vec2 worldToScreen(glm::vec2 originWorld, uint32 x, uint32 y, uint32 size) { */
-/*     float32 screenX = (originWorld.x * size) + (x - y) * (size / 2); */
-/*     float32 screenY = (originWorld.y * size) + (x + y) * (size / 2); */
+void UpdateAndRender(Game* game, SpriteRenderer* renderer, Plateform_MouseEvent* mouse) {
+	// @improve: Should avoid as many drawcalls! Batch rendering?
+    for(std::size_t i = 0; i < game->testList.size(); ++i)
+        DrawSprite(renderer, &game->testList[i], GetShaderFromCache(SHADER_CACHE, "sprite"));
 
-/*     return glm::vec2(screenX, screenY); */
-/* } */
+    glm::vec2 mouseWorld = screenToWorld(
+        (float32)(mouse->posX - game->origin.x),
+        (float32)(mouse->posY - game->origin.y),
+        (uint32)game->selectedTest.size.x);
+    printf("mouse world %.1f/%.1f\n", mouseWorld.x, mouseWorld.y);
 
-glm::vec2 worldToScreen(uint32 x, uint32 y, uint32 tileSize) {
-    glm::vec2 screenPos(0.0f);
+    // Rendere selected sprite
+    if ((mouseWorld.x >= 0 && mouseWorld.y >= 0) && (mouseWorld.x < game->world.x && mouseWorld.y < game->world.y)) {
+        glm::vec2 selectedSprite = worldToScreen((uint32)mouseWorld.x, (uint32)mouseWorld.y, (uint32)game->selectedTest.size.x);
+        game->selectedTest.position.x = game->origin.x + selectedSprite.x;
+        game->selectedTest.position.y = game->origin.y + selectedSprite.y;
 
-    float32 posX = (float32)(x*tileSize);
-    float32 posY = (float32)(y*tileSize);
-
-    screenPos.x = (posX - posY) / 2;
-    screenPos.y = (posX + posY) / 4;
-
-    return screenPos;
+        DrawSprite(renderer, &game->selectedTest, GetShaderFromCache(SHADER_CACHE, "sprite"));
+    }
 }
 
-glm::vec2 worldToScreen2(uint32 x, uint32 y, uint32 tileSize) {
+glm::vec2 worldToScreen(uint32 x, uint32 y, uint32 tileSize) {
     glm::vec2 screenPos(0.0f);
 
     screenPos.x = ((float32)(x * 0.5 * tileSize) + (float32)(y * (-0.5) * tileSize)) - (float32)(tileSize / 2);
@@ -97,58 +133,13 @@ glm::vec2 worldToScreen2(uint32 x, uint32 y, uint32 tileSize) {
 }
 
 glm::vec2 screenToWorld(float32 x, float32 y, uint32 tileSize) {
-    glm::vec2 worldPos(0.0f);
+    glm::ivec2 world(0);
 
-    worldPos.x = (float32)(0.5 * (x / (tileSize/2) + y / (tileSize/4)));
-    worldPos.y = (float32)(0.5 * (-x / (tileSize/2) + y / (tileSize/4)));
+    int32 wx = (int32)(0.5 * (x / (tileSize/2) + y / (tileSize/4)));
+    int32 wy = (int32)(0.5 * (-x / (tileSize/2) + y / (tileSize/4)));
 
-    return worldPos;
-}
+    world.x = (int32)glm::floor(wx);
+    world.y = (int32)glm::floor(wy);
 
-void UpdateAndRender(Game* game, SpriteRenderer* renderer, Plateform_MouseEvent* mouse) {
-    std::vector<Sprite> testList;
-    testList.clear();
-
-    uint32 sizeX = 10;
-    uint32 sizeY = 14;
-    uint32 spriteSize = 100;
-    glm::vec2 origin = glm::vec2((float32)(game->width/2), (float32)(game->height/3));
-    //glm::vec2 origin = glm::vec2(0.0f);
-
-
-    glm::vec2 cell = glm::vec2((float32)(mouse->posX / spriteSize), (float32)(mouse->posY / spriteSize));
-
-    for (uint32 y=0; y<sizeY; y++) {
-        for (uint32 x=0; x<sizeX; x++) {
-            glm::vec2 screenPos = worldToScreen2(x, y, spriteSize);
-            testList.push_back(InitSprite(GetTextureFromCache(TEXTURE_CACHE, "block"),
-                                          glm::vec2(origin.x + screenPos.x, origin.y + screenPos.y),
-                                          glm::vec2((float32)spriteSize),
-                                          glm::vec3(1.0f)));
-        }
-    }
-
-
-
-// @improve: Should avoid as many drawcalls! Batch rendering?
-    for(std::size_t i = 0; i < testList.size(); ++i)
-        DrawSprite(renderer, &testList[i], GetShaderFromCache(SHADER_CACHE, "sprite"));
-
-    glm::vec2 test = screenToWorld((float32)(mouse->posX - origin.x), (float32)(mouse->posY - origin.y), spriteSize);
-    printf("world %.1f/%.1f\n", test.x, test.y);
-
-    int32 fx = (int32)glm::floor(test.x);
-    int32 fy = (int32)glm::floor(test.y);
-    printf("floor %d/%d\n", fx, fy);
-
-    if ((fx >= 0 && fy >= 0) && (fx < (int32)sizeX && fy < (int32)sizeY)) {
-        glm::vec2 selectedCellPos = worldToScreen2(fx, fy, spriteSize);
-        printf("screen %.1f/%.1f\n", selectedCellPos.x, selectedCellPos.y);
-
-        Sprite selected = InitSprite(GetTextureFromCache(TEXTURE_CACHE, "block5"),
-                                     glm::vec2(origin.x + selectedCellPos.x, origin.y + selectedCellPos.y),
-                                     glm::vec2((float32)spriteSize),
-                                     glm::vec3(1.0f));
-        DrawSprite(renderer, &selected, GetShaderFromCache(SHADER_CACHE, "sprite"));
-    }
+    return world;
 }
